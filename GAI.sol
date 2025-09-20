7cdf04e4be3bb25adcdf9a08c4dda2a71d465a9b
JAEHOONYOO-GAI <juehun101@gmail.com>
2025-09-20 23:39:35 +0900

    # GAI Token (GearAI)
    - Symbol: GAI
    - Total Supply: 50,000,000
    - Standard: ERC-20
    - Network: Polygon
    - Verified on Polygonscan

---
+++ GAI.sol
@@ -0,0 +1,46 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract GAI {
+    string public name = "GearAI";
+    string public symbol = "GAI";
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    constructor() {
+        totalSupply = 50000000 * (10 ** uint256(decimals));
+        balanceOf[msg.sender] = totalSupply;
+        emit Transfer(address(0), msg.sender, totalSupply);
+    }
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value, "Insufficient balance");
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[_from] >= _value, "Insufficient balance");
+        require(allowance[_from][msg.sender] >= _value, "Allowance exceeded");
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+}
